8502b1709370b5d12cc4fa552d592d3eaf491350
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2014-07-01 09:03:31 +0200

    CordovaBootstrapKnockoutDemo: renamed "js" to "scripts"

---
+++ Demos/CordovaBootstrapKnockoutDemo/CordovaBootstrapKnockoutDemo/scripts/getData.js
@@ -0,0 +1,27 @@
+var getData = function () {
+
+    var url = 'http://johanneshoppe.github.io/DeveloperMediaSlides/examples/webinarp.json';
+    var urlFallback = 'scripts/talks_fallback.json';
+
+    var request = $.Deferred(function (deferred) {
+
+        $.ajax({
+            url: url,
+            dataType: 'jsonp',
+            jsonpCallback: 'callback',
+            timeout: 2000,
+        })
+        .done(function (data, textStatus, jqXHR) {
+            deferred.resolveWith(this, [data, textStatus, jqXHR]);
+        })
+        .fail(function (jqXHR, textStatus) {
+
+            jQuery.getJSON(urlFallback, function (data) {
+                deferred.resolveWith(this, [data, textStatus, jqXHR]);
+            });
+
+        });
+    }).promise();
+
+    return request;
+}
