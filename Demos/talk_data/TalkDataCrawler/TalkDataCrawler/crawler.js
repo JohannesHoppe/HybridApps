fa3cf15bf16afa1717205cb481ed9fbac69a7350
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2014-07-03 09:02:07 +0200

    work in progress

--- Demos/talk_data/TalkDataCrawler/TalkDataCrawler/crawler.js
+++ Demos/talk_data/TalkDataCrawler/TalkDataCrawler/crawler.js
@@ -1,29 +1,60 @@
 /**
  * A qick & dirty crawler to get the events from DWX 2014
+ *
+ * Note: npm ncrawler will require you to install Python first
  */
 
-var Crawler = require("crawler").Crawler,
+var fs = require('fs'),
+    Crawler = require("crawler").Crawler,
     config = {
         startNumber: 14107,
-        endNumber: 15107,
-        url: 'http://www.developer-week.de/Programm/Veranstaltung/(event)/'
+        endNumber: 14108,
+        url: 'http://www.developer-week.de/Programm/Veranstaltung/(event)/',
+        fileNameJson: 'talks_fallback.json',
+        fileNameJsonP: 'talks_callback.json',
+    },
+    talks = [];
+
+var crawlerCallback = function(error, result, $) {
+
+    var title = $(".container h2:first").text();
+    var description = $(".container p").text();
+    var date = $(".container .ezagenda_date").text();
+
+    talks.push({
+        title: title,
+        description: description,
+        date: date
+    });
+};
+
+var crawlerOnDrain = function() {
+
+    var talksJson = JSON.stringify(talks);
+    var talksJsonP = 'callback(' + talksJson + ');';
+
+    fs.writeFile(config.fileNameJson, talksJson, 'utf8', console.log);
+    fs.writeFile(config.fileNameJsonP, talksJsonP, 'utf8', console.log);
+};
+
+var crawlerStart = function() {
+
+    var crawler, urls = [],
+        i = config.startNumber;
+
+    while (i <= endNumber) {
+        urls.push(url + ++i);
     }
-    urls = [],
-    i = config.startNumber;
 
-while (i <= endNumber) {
-    urls.push(url + ++i);
-}
+    crawler = new Crawler({
+        "maxConnections": 10,
+        "callback": crawlerCallback,
+        "onDrain": crawlerOnDrain
+    });
 
-var crawler = new Crawler({
-    "maxConnections": 10,
-    "callback": function(error, result, $) {
+    crawler.queue(urls);
+};
 
-        var title = $(".container h2:first").text();
-        var description = $(".container p").text();
-        var date = $(".container .ezagenda_date").text();
+crawlerStart();
 
-    }
-});
 
-crawler.queue(urls);
