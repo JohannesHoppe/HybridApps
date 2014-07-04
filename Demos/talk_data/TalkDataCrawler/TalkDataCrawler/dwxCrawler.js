1ae93cb565886955314092a88aefa9eb9e1cb868
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2014-07-04 17:22:33 +0200

    coded a crawler without any internet connection... fuck you deutsche bahn!

---
+++ Demos/talk_data/TalkDataCrawler/TalkDataCrawler/dwxCrawler.js
@@ -0,0 +1,59 @@
+﻿var jsdom = require('jsdom'),
+    jquery = require('jquery'),
+    crawler = require("simplecrawler"),
+    Crawler;
+
+Crawler = function(completeCallback) {
+
+    this.talks = [];
+    this.completeCallback = completeCallback;
+}
+
+Crawler.prototype._jQuerifyHtml = function(html) {
+    
+    var window = jsdom.jsdom(html, null, {
+        FetchExternalResources: false,
+        ProcessExternalResources: false,
+        MutationEvents: false,
+        QuerySelector: false
+    }).createWindow();
+ 
+    return jquery.create(window);
+}
+
+Crawler.prototype._fetchcomplete = function(queueItem , responseBuffer , response) {
+
+    console.log("Completed fetching resource:", queueItem.url);
+
+    if (queueItem.url.indexOf("(event)") == -1) {
+        return;
+    }
+
+    var $ = this._jQuerifyHtml(response);
+
+    var title = $(".container h2:first").text();
+    var description = $(".container p").text();
+    var date = $(".container .ezagenda_date").text();
+
+    if (!title || !date || date.indexOf("2014") == -1) {
+        return;
+    }
+
+    this.talks.push({
+        title: title,
+        description: description,
+        date: date
+    });
+};
+
+Crawler.prototype.crawlerStart = function() {
+
+    crawler.crawl(config.url)
+        .on("fetchcomplete", this._fetchcomplete.bind(this))
+        .on("complete", function() {
+            this.completeCallback(this.talks);
+        }.bind(this));
+
+};
+
+module.exports = Crawler;
---
+++ Demos/talk_data/TalkDataCrawler/TalkDataCrawler/start.js
@@ -0,0 +1,23 @@
+/**
+ * A simple crawler to get the events from DWX 2014
+ */
+
+var fs = require('fs'),
+    DwxCrawler = require('dwxCrawler'), dwxCrawler,
+    saveTalksToDisk,
+
+    config = {
+        url: 'http://www.developer-week.de/Programm',
+        fileNameJson: 'talks_fallback.json',
+        fileNameJsonP: 'talks_callback.json',
+    };
+
+dwxCrawler = new DwxCrawler();
+dwxCrawler.crawlerStart(function(talks) {
+
+    var talksJson = JSON.stringify(talks);
+    var talksJsonP = 'callback(' + talksJson + ');';
+
+    fs.writeFile(config.fileNameJson, talksJson, 'utf8', console.log);
+    fs.writeFile(config.fileNameJsonP, talksJsonP, 'utf8', console.log);
+});
