e8640f76444cdd695df277e5567ec33232454d6d
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2014-07-06 22:12:31 +0200

    fixed crawler

--- Demos/TalkDataCrawler/talks_fallback_copy.cmd
+++ Demos/TalkDataCrawler/talks_fallback_copy.cmd
@@ -3,8 +3,8 @@
 %~d0
 CD "%~dp0"
 
-copy talks_fallback.json ..\CordovaBootstrapKnockoutDemo\CordovaBootstrapKnockoutDemo\scripts\
+copy talks_fallback.json ..\CordovaBootstrapKnockout\CordovaBootstrapKnockout\scripts\
 copy talks_fallback.json ..\CordovajQueryMobile\CordovajQueryMobile\scripts\
-copy talks_fallback.json ..\CordovaKendoUiMobileDemo\CordovaKendoUiMobileDemo\scripts\
+copy talks_fallback.json ..\CordovaKendoUiMobile\CordovaKendoUiMobile\scripts\
 
 pause
