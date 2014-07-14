f2a5d5bc6f625251e54ba1f801646c71388a5467
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2014-07-14 23:29:12 +0200

    Live Coding: prepare empty version

---
+++ Demos/CordovaBootstrapKnockout - Vorher/CordovaBootstrapKnockout/scripts/IndexPageViewModel.js
@@ -0,0 +1,29 @@
+﻿var IndexPageViewModel = (function (ko) {
+
+    var IndexPageViewModel = function () {
+
+        var self = this;
+
+        self.talks = ko.observableArray();
+        self.selectedNote = ko.observable(false);
+
+        self.loadData = function () {
+
+            self.talks.removeAll();
+            getData().done(function(result) {
+                self.talks = ko.mapping.fromJS(result, {}, self.talks);
+            });
+        };
+
+        self.showDetails = function (current) {
+            self.selectedNote(current);
+        };
+
+        self.showHome = function() {
+            self.selectedNote(false);
+        };
+    };
+
+    return IndexPageViewModel;
+    
+})(window.ko);
